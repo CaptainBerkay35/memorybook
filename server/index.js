0be012a5638bdbad3aad1cc290dfaa96c2c7937f
Berkay Kaptan <kaptanberkay35@hotmail.com>
2025-05-20 13:08:22 +0300

    env materials added

--- server/index.js
+++ server/index.js
@@ -2,11 +2,13 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
 
 
 const app = express();
+dotenv.config();
 
 app.use(express.json({ limit: "30mb" }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
@@ -15,12 +17,10 @@ app.use(cors());
 app.use('/posts',postRoutes);
 
 
-const CONNECTION_URL =
-  "mongodb+srv://kaptanberkay35:lkPtMnVmIBjYjUC6@cluster0.nt2htoc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(CONNECTION_URL)
+  .connect(process.env.CONNECTION_URL)
   .then(() => app.listen(PORT, () => console.log(`Server running on PORT: ${PORT}`)))
   .catch((error) => console.log(error.message));
 
